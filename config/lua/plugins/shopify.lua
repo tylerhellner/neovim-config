a29cf29a9156d3d822029ece9e854cf0bf541361
Tyler Hellner <thellner06@gmail.com>
2023-05-09 14:12:44 -0700

    try to debug shopify theme check to no avail

--- config/lua/plugins/shopify.lua
+++ config/lua/plugins/shopify.lua
@@ -1,10 +1,4 @@
 return {
-  { --MASON
-    "williamboman/mason.nvim",
-    opts = function(_, opts)
-      table.insert(opts.ensure_installed, "shopify-theme-check")
-    end,
-  },
   {
     "neovim/nvim-lspconfig",
     opts = {
