dcdc71dbc5c7bb430bf858a2b69220a5238c15fd
Tyler Hellner <thellner06@gmail.com>
2023-06-22 11:57:06 -0700

    fix: remove transparency from and adds border to autocompletion overlay

--- config/lua/config/options.lua
+++ config/lua/config/options.lua
@@ -2,4 +2,13 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
+-- Disable Transparency on LSP Intellisense Suggestions
+vim.opt.pumblend = 0
+
+-- Change leader character from \ to something else
 vim.g.mapleader = ","
+
+-- Adds Border to LSP Intellisense Suggestions
+local float = { focusable = true, style = "minimal", border = "rounded" }
+vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, float)
+vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, float)
