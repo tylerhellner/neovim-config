fbbad0ad9c6d652db1d4097f74a5e9dd1f2c6ada
Tyler Hellner <thellner06@gmail.com>
2023-08-11 12:39:59 -0700

    feat(svelte): :wrench: insure svelte formatter is installed for TS completion

--- config/lua/plugins/typescript.lua
+++ config/lua/plugins/typescript.lua
@@ -3,9 +3,9 @@ return {
   {
     "nvim-treesitter/nvim-treesitter",
     opts = function(_, opts)
-      -- Typescript Completion
+      -- Typescript Completion, Svelte
       if type(opts.ensure_installed) == "table" then
-        vim.list_extend(opts.ensure_installed, { "typescript", "tsx" })
+        vim.list_extend(opts.ensure_installed, { "typescript", "tsx", "svelte" })
       end
     end,
   },
@@ -22,6 +22,7 @@ return {
     opts = {
       -- make sure mason installs the server
       servers = {
+        svelte = {},
         ---@type lspconfig.options.tsserver
         tsserver = {
           settings = {
