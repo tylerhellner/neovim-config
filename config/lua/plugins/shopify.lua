2e6b6b0169fdc68f063cea65b5e4d224d5b8e594
Tyler Hellner <thellner06@gmail.com>
2023-08-11 12:47:55 -0700

    refactor(shopify-config): :recycle: :adhesive_bandage: clean up shopify config

--- config/lua/plugins/shopify.lua
+++ config/lua/plugins/shopify.lua
@@ -1,10 +1,25 @@
 return {
   {
     "neovim/nvim-lspconfig",
+    ---@class PluginLspOpts
     opts = {
+      ---@type lspconfig.options
       servers = {
         theme_check = {},
       },
     },
+  },
+  {
+    "williamboman/mason.nvim",
+    opts = function(_, opts)
+      table.insert(opts.ensure_installed, "prettierd")
+    end,
+  },
+  { --NULL-LS
+    "jose-elias-alvarez/null-ls.nvim",
+    opts = function(_, _)
+      local null_ls = require("null-ls")
+      null_ls.builtins.formatting.prettierd.with({ extra_filetypes = { "liquid" } })
+    end,
   },
 }
