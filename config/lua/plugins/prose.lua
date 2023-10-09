6eb81ab60e8d3c45dadbfa27723dc8e60bfc504e
Tyler Hellner <thellner06@gmail.com>
2023-10-09 11:56:38 -0700

    feat(plugin): adds nvim-surround

--- config/lua/plugins/prose.lua
+++ config/lua/plugins/prose.lua
@@ -1,10 +1,24 @@
-return { -- NULL-LS
-  "nvimtools/none-ls.nvim",
-  opts = function(_, opts)
-    local null_ls = require("null-ls")
-    table.insert(opts.sources, null_ls.builtins.diagnostics.markdownlint_cli2)
-    table.insert(opts.sources, null_ls.builtins.diagnostics.alex)
-    table.insert(opts.sources, null_ls.builtins.code_actions.proselint)
-    table.insert(opts.sources, null_ls.builtins.diagnostics.proselint)
-  end,
+return {
+  {
+    -- NeoVim Surround
+    "kylechui/nvim-surround",
+    version = "*", -- Use for stability; omit to use `main` branch for the latest features
+    event = "VeryLazy",
+    config = function()
+      require("nvim-surround").setup({
+        -- Configuration here, or leave empty to use defaults
+      })
+    end,
+  },
+  {
+    -- NULL-LS
+    "nvimtools/none-ls.nvim",
+    opts = function(_, opts)
+      local null_ls = require("null-ls")
+      table.insert(opts.sources, null_ls.builtins.diagnostics.markdownlint_cli2)
+      table.insert(opts.sources, null_ls.builtins.diagnostics.alex)
+      table.insert(opts.sources, null_ls.builtins.code_actions.proselint)
+      table.insert(opts.sources, null_ls.builtins.diagnostics.proselint)
+    end,
+  },
 }
