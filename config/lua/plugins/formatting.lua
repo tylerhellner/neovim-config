d5a92448abc93bde9630eca4adc3012f98296c7e
Tyler Hellner <thellner06@gmail.com>
2023-12-16 12:24:46 -0800

    fix(headliner): removes missing characters in headliner

--- config/lua/plugins/formatting.lua
+++ config/lua/plugins/formatting.lua
@@ -7,4 +7,14 @@ return {
       },
     },
   },
+  -- headliner formats markdown headings and other markdown items.
+  {
+    "lukas-reineke/headlines.nvim",
+    dependencies = "nvim-treesitter/nvim-treesitter",
+    opts = {
+      markdown = {
+        fat_headline_lower_string = "▔",
+      },
+    },
+  },
 }
