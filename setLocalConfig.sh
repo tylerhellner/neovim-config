e41a36634cd97adc34ba3c15073a36ecae12e350
Tyler Hellner <thellner06@gmail.com>
2023-12-28 16:37:55 -0500

    fix: folder copied to wrong location

--- setLocalConfig.sh
+++ setLocalConfig.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 # Copies this config to your local nvim directory.
-rsync -havP --exclude '.DS_Store' ./config/lua ~/.config/nvim/lua
+rsync -havP --exclude '.DS_Store' ./config/lua ~/.config/nvim
 echo All Done!
