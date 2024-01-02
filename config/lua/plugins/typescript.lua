0582b1f07030ea3e2cd69cd4438dbfc6cf618eaf
Tyler Hellner <thellner06@gmail.com>
2024-01-02 15:32:13 -0800

    config(mason): adds auto-install configs for astro
    Uses Mason and Tresitter

--- config/lua/plugins/typescript.lua
+++ config/lua/plugins/typescript.lua
@@ -1,11 +1,14 @@
 return {
   {
     "nvim-treesitter/nvim-treesitter",
-    opts = function(_, opts)
-      -- Typescript Completion, Svelte
-      if type(opts.ensure_installed) == "table" then
-        vim.list_extend(opts.ensure_installed, { "typescript", "tsx", "svelte" })
-      end
-    end,
+    opts = {
+      ensure_installed = { "typescript", "tsx", "svelte", "astro" },
+    },
+  },
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = { "astro-language-server" },
+    },
   },
 }
